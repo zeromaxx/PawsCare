37800526c040595e0a0e3d033703cd5086fb3ffa
arislamp <129835271+arislamp@users.noreply.github.com>
2024-04-21 16:24:50 +0300

    i

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,10 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿$(document).ready(function () {
+    $("#toggleButton").click(function () {
+        var sidebarWidth = $("#sidebar").width();
+        if (sidebarWidth > 0) {
+            $("#sidebar").width(0);
+        } else {
+            $("#sidebar").width('100%');
+        }
+    });
+});
